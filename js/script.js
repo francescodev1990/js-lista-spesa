e459fb97cfe38d9612f5a311edff3601100d9984
Your Name <almannofrancesco@gmail.com>
2022-12-06 08:44:51 +0100

    last modified to finished

--- js/script.js
+++ js/script.js
@@ -10,18 +10,9 @@ const list = [
     'Lenticchie'
 ];
 
-// console.log(list);
 
 const container = document.querySelector('.lista-nonstilizzata');
 
-
-// let element = '';
-
-// for(let i = 0; i < list.length; i++){
-//     let item = list[i];
-//     console.log(item);
-//     element += `<li class="list-item">${item}</li>`;
-//     container.innerHTML = element;
 
 let i = 0 ;
 let element = ''
