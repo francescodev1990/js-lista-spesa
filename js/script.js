6bcd541bd553a1eb31f75a1908260fb726bc89bd
Your Name <almannofrancesco@gmail.com>
2022-12-06 08:44:13 +0100

    js fix and completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-alert('Ciao!');
+const list = [
+    'Caramelle',
+    'Gomme',
+    'Pasta',
+    'Verdura',
+    'Fagioli',
+    'Pizza',
+    'Piselli',
+    'Mais',
+    'Lenticchie'
+];
+
+// console.log(list);
+
+const container = document.querySelector('.lista-nonstilizzata');
+
+
+// let element = '';
+
+// for(let i = 0; i < list.length; i++){
+//     let item = list[i];
+//     console.log(item);
+//     element += `<li class="list-item">${item}</li>`;
+//     container.innerHTML = element;
+
+let i = 0 ;
+let element = ''
+while(i < list.length){
+let item = list[i];
+element += `<li class="list-item">${item}</li>`;
+container.innerHTML = element;
+i++;
+}
+console.log(list);
